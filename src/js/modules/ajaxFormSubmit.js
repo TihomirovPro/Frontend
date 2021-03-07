884da5b37df05c6cef00e6ecb0aff2e8fb5b164c
Tihomirov.pro <tihomirov.pro@gmail.com>
2021-03-07 23:46:18 +0300

    update

---
+++ src/js/modules/ajaxFormSubmit.js
@@ -0,0 +1,31 @@
+const ajaxFormSubmit = () => {
+  const forms = document.forms
+
+  forms.forEach(form => {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      const formData = new FormData(form)
+      postData('./mail.php', formData)
+        .then(res => successSend())
+        .catch(() => errorSend())
+        .finally(() => finallySend())
+    })
+  })
+}
+
+const postData = async (url, data) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    body: data
+  })
+  return await res.text()
+}
+
+const successSend = () => {}
+
+const errorSend = () => {}
+
+const finallySend = () => {}
+
+export default ajaxFormSubmit
