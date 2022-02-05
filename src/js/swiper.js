e2cb6b27a011f2782457fba3fce35c212fa4033e
Sergey Tikhomirov <tihomirov.pro@gmail.com>
2022-02-05 17:38:43 +0300

    update

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,5 +1,12 @@
-import Swiper from 'swiper/bundle'
+/* eslint-disable no-unused-vars */
+
+import Swiper from 'swiper'
+// import Swiper, { Navigation, Pagination } from 'swiper'
 
 document.addEventListener('DOMContentLoaded', () => {
-  //
+  const swiper = new Swiper('.swiper', {
+    // modules: [Navigation, Pagination]
+    slidesPerView: 1,
+    spaceBetween: 10
+  })
 })
